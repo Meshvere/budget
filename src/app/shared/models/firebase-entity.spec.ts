b25f0e64aba5f34fff4a4e0890bb930bd9df3690
Stéphane Ruau <sruau@sma-netagis.fr>
2020-10-11 00:17:24 +0200

    backup add back

--- src/app/shared/models/firebase-entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FirebaseEntity } from './firebase-entity';
-
-describe('FirebaseEntity', () => {
-  it('should create an instance', () => {
-    expect(new FirebaseEntity()).toBeTruthy();
-  });
-});
